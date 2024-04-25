15a93741b77c63f25b9efde0ca5383fa9e038674
blackcat13 <detective.blackcat13@gmail.com>
2024-04-25 20:54:04 +0700

    add Image Form by React Form and Zod

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
 
-const AddTransformationTypePage = () => {
+import { transformationTypes } from "@/constants"
+import TransformationForm from "@/components/shared/TransformationForm"
+import Header from "@/components/shared/Header"
+import { auth } from "@clerk/nextjs/server"
+import { getUserById } from "@/lib/actions/user.action"
+import { redirect } from "next/navigation"
+
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type]
+
+  if (!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId);
+
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header
+        title={transformation.title}
+        subTitle={transformation.subTitle}
+      />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
